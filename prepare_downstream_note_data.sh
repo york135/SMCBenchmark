2274355a62858ddeed5955260f05edc9d6c1885f
york135 <junyou.wang@mirlab.org>
2025-06-04 17:15:37 +0800

    Uncommented bash script

--- prepare_downstream_note_data.sh
+++ prepare_downstream_note_data.sh
@@ -1,31 +1,31 @@
 # MNID
-# python3 get_downstream_note_dataset.py \
-# --dataset=bps_motif --task=mnid --output_dir downstream_note/
+python3 get_downstream_note_dataset.py \
+--dataset=bps_motif --task=mnid --output_dir downstream_note/
 
 # melody
-# python3 get_downstream_note_dataset.py \
-#  --dataset=pop909 --task=melody --output_dir downstream_note/
+python3 get_downstream_note_dataset.py \
+ --dataset=pop909 --task=melody --output_dir downstream_note/
 
 # velocity
-# python3 get_downstream_note_dataset.py \
-#  --dataset=pop909 --task=velocity --output_dir downstream_note/
+python3 get_downstream_note_dataset.py \
+ --dataset=pop909 --task=velocity --output_dir downstream_note/
 
 # OTC
-# python3 get_downstream_note_dataset.py \
-#   --dataset=orch --task=texture --output_dir downstream_note/
+python3 get_downstream_note_dataset.py \
+  --dataset=orch --task=texture --output_dir downstream_note/
 
 # Augnet (chord, localkey)
-# python3 get_downstream_note_dataset.py \
-#  --dataset=augnet --task=chordroot --output_dir downstream_note/
+python3 get_downstream_note_dataset.py \
+ --dataset=augnet --task=chordroot --output_dir downstream_note/
 
-# python3 get_downstream_note_dataset.py \
-#  --dataset=augnet --task=localkey --output_dir downstream_note/
+python3 get_downstream_note_dataset.py \
+ --dataset=augnet --task=localkey --output_dir downstream_note/
 
-# python3 get_downstream_note_dataset.py \
-#  --dataset=pm2s --task=beat --output_dir downstream_note/
+python3 get_downstream_note_dataset.py \
+ --dataset=pm2s --task=beat --output_dir downstream_note/
 
-# python3 get_downstream_note_dataset.py \
-#  --dataset=pm2s --task=downbeat --output_dir downstream_note/
+python3 get_downstream_note_dataset.py \
+ --dataset=pm2s --task=downbeat --output_dir downstream_note/
 
 python3 get_downstream_note_dataset.py \
  --dataset=tnua --task=violin_all --output_dir downstream_note/
